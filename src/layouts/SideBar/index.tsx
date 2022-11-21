17a989185cd2bc5b7028e23dccf5ed758f39651d
toilakien <kienle17042001@gmail.com>
2022-11-21 15:09:10 +0700

    update

--- src/layouts/SideBar/index.tsx
+++ src/layouts/SideBar/index.tsx
@@ -1,92 +1,104 @@
-import { Layout, Menu, MenuProps } from 'antd';
-import React from 'react'
+import { Layout, Menu, MenuProps } from "antd";
+import React from "react";
 import {
-    LaptopOutlined,
-    NotificationOutlined,
-    UserOutlined,
-    CarOutlined
+  LaptopOutlined,
+  NotificationOutlined,
+  UserOutlined,
+  CarOutlined,
 } from "@ant-design/icons";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 const SideBar = () => {
-    const basenav = [
+  const basenav = [
+    {
+      icon: UserOutlined,
+      label: "Dashbound",
+      children: [
         {
-            icon: UserOutlined,
-            label: "Dashbound",
-            children: [
-                {
-                    icon: <Link to="/dashbound"><CarOutlined /></Link>,
-                    label: "Dashbound",
-                },
-            ]
+          icon: (
+            <Link to="/dashbound">
+              <CarOutlined />
+            </Link>
+          ),
+          label: "Dashbound",
+        },
+      ],
+    },
+    {
+      icon: UserOutlined,
+      label: "Sản phẩm",
+      children: [
+        {
+          icon: (
+            <Link to="/product">
+              <CarOutlined />
+            </Link>
+          ),
+          label: "Ô tô",
         },
         {
-            icon: UserOutlined,
-            label: "Sản phẩm",
-            children: [
-                {
-                    icon: <Link to="/product"><CarOutlined /></Link>,
-                    label: "Ô tô",
-                },
-                {
-                    icon: <Link to="/category"> < LaptopOutlined /></Link>,
-                    label: "Xe máy",
-                },
-                {
-                    icon: <Link to=""></Link>,
-                    label: "Xe Điện",
-                },
-            ]
+          icon: (
+            <Link to="/category">
+              {" "}
+              <LaptopOutlined />
+            </Link>
+          ),
+          label: "Xe máy",
         },
         {
-            icon: LaptopOutlined,
-            label: "Danh mục",
-            children: [
-                {
-                    icon: <Link to="/category"></Link>,
-                    label: "New",
-                },
-                {
-                    icon: <Link to=""></Link>,
-                    label: "770",
-                },
-                {
-                    icon: <Link to=""></Link>,
-                    label: "991",
-                },
-            ]
+          icon: <Link to=""></Link>,
+          label: "Xe Điện",
         },
+      ],
+    },
+    {
+      icon: LaptopOutlined,
+      label: "Danh mục",
+      children: [
+        {
+          icon: <Link to="/category"></Link>,
+          label: "New",
+        },
+        {
+          icon: <Link to=""></Link>,
+          label: "770",
+        },
+        {
+          icon: <Link to=""></Link>,
+          label: "991",
+        },
+      ],
+    },
+  ];
+  const { Sider } = Layout;
+  const items2: MenuProps["items"] = basenav.map((base, index) => {
+    const key = String(index + 1);
 
-    ]
-    const { Sider } = Layout;
-    const items2: MenuProps["items"] = basenav.map((base, index) => {
-        const key = String(index + 1);
+    return {
+      key: key,
+      icon: React.createElement(base.icon),
+      label: base.label,
 
+      children: base.children.map((element, index) => {
         return {
-            key: key,
-            icon: React.createElement(base.icon),
-            label: base.label,
-
-            children: base.children.map((element, index) => {
-                return {
-                    key: index,
-                    label: element.label,
-                    icon: element.icon
-                };
-            })
+          key: index,
+          label: element.label,
+          icon: element.icon,
         };
-    });
+      }),
+    };
+  });
 
-    return (
-        <Sider width={200} style={{ background: "#fff" }}>
-            <Menu
-                mode="inline"
-                // defaultSelectedKeys={["1"]}
-                // defaultOpenKeys={["sub1"]}
-                style={{ height: "100%", borderRight: 0 }}
-                items={items2}
-            />
-        </Sider>
-    )
-}
+  return (
+    <Sider width={200} style={{ background: "#fff" }}>
+      <Menu
+        mode="inline"
+        // defaultSelectedKeys={["1"]}
+        // defaultOpenKeys={["sub1"]}
+        style={{ height: "100%", borderRight: 0 }}
+        items={items2}
+      />
+    </Sider>
+  );
+};
 
-export default SideBar
+export default SideBar;
